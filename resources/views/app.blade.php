e5292da88b84eda6a439f7bee36c36dc64d23559
hewhounknown <zwemarnhtay@gmail.com>
2023-11-08 06:55:57 +0630

    include languages intro

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,7 @@
                             </a>
                             <ul class="dropdown-menu">
                                 <li><a class="dropdown-item" href="{{route('languages')}}">Languages</a></li>
-                                <li><a class="dropdown-item" href="#">ITPEC</a></li>
+                                <li><a class="dropdown-item" href="#">IT</a></li>
                             </ul>
                         </li>
                         <li class="nav-item">
--- resources/views/programmes/languages.blade.php
+++ resources/views/programmes/languages.blade.php
@@ -3,9 +3,20 @@
 @section('title', 'languages')
 
 @section('content')
-    <div class="container">
-        <div class="row bg-danger" style="background-image: url({{asset('img/p2.jpg')}}); background-size: cover; width: 100%; height: 100%;">
-            <h1>hhh</h1>
+    <div class="container-fluid" style="">
+        {{-- <div class="row bg-danger" style=" height:auto;
+        background: url({{asset('img/p3.jpg')}});
+        background-repeat: no-repeat; background-size: 100%"> --}}
+        <div
+        class="bg-image p-5 shadow-1-strong rounded mb-5 text-white"
+        style="background: url({{asset('img/globe.png')}});  width:100wh; height:60vh;
+               background-color: #E6E6FA; background-position: bottom 50px right 30px; background-repeat: no-repeat;">
+            <div class="row">
+                <div class="mt-5 col-6 text-dark align-self-center">
+                    <h2>Languages...</h2>
+                    <h5 class="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus vel, officiis cupiditate quod saepe est laborum ducimus earum, ex dicta consequatur perspiciatis reprehenderit hic, animi expedita fugit quae! Rem, aliquam.</h5>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
