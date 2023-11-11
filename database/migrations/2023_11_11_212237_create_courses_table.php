2328fe3dae2ffcd2013da039fba406b19cf57290
hewhounknown <zwemarnhtay@gmail.com>
2023-11-12 05:01:05 +0630

    DB

---
+++ database/migrations/2023_11_11_212237_create_courses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique();
+            $table->text('description');
+            $table->text('content');
+            $table->string('photo_path')->nullable();
+            $table->string('video_path')->nullable();
+            $table->string('language_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
