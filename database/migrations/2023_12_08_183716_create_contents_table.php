d7a1acaf70a65aa02cc4aa3462cfb6e904a6c3ef
hewhounknown <zwemarnhtay@gmail.com>
2023-12-09 01:55:50 +0630

    courseDetail Day4 including add text content

--- database/migrations/2023_12_08_183716_create_contents_table.php
+++ database/migrations/2023_12_08_183716_create_contents_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('contents', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->enum('content_type', ['text', 'image', 'video']);
-            $table->string('content_path');
+            $table->enum('content_type', ['text', 'video', 'image', 'file']);
+            $table->text('content_body')->nullable();
+            $table->string('content_path')->nullable();
             $table->integer('topic_id');
             $table->timestamps();
         });
