f498d3899959d0e106d53b0373eb2da348a1612a
hewhounknown <zwemarnhtay@gmail.com>
2024-02-20 00:37:39 +0630

    create topic modal done

--- resources/views/admin/coursedetail.blade.php
+++ resources/views/admin/coursedetail.blade.php
@@ -2,6 +2,70 @@
 
 @section('content')
     course detail
+
+    @if ($course->teacher_id == Auth::user()->id)
+    <div class="row justify-content-end">
+        <div class="col-3 text-center">
+            <div class="btn btn-outline-secondary btn-lg" data-bs-toggle="modal" data-bs-target="#topicModal">
+                <i class="fa-solid fa-file-medical"></i>
+            </div>
+        </div>
+    </div>
+
+    {{--topic modal start--}}
+    <div class="modal fade" id="topicModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Topic Create</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form enctype="multipart/form-data" action="" method="post">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-2">
+                        <label for="topicName">Topic Name</label>
+                        <input type="text" name="topicName" id="" class="form-control">
+                    </div>
+                    <div class="mb-2">
+                        <label for="topicDescription">Topic Description</label>
+                        <textarea name="topicDescription" class="form-control" id="" cols="30" rows="5"></textarea>
+                    </div>
+                    <hr>
+
+                    <div class="mb-2">
+                        <label for="contentTitle">Content Title</label>
+                        <input type="text" name="contentTitle" id="" class="form-control">
+                    </div>
+                    <div class="mb-2">
+                        <label for="contentType">Content Type</label>
+                        <select id="selectContentType" name="contentType" class="form-select">
+                            <option value="text">Text</option>
+                            <option value="file">File</option>
+                            <option value="image">Image</option>
+                            <option value="video">Video</option>
+                        </select>
+                    </div>
+                    <div class="mb-2" id="textArea">
+                        <label for="contentBody">Content</label>
+                        <textarea name="contentBody" class="form-control" id="textContent" cols="30" rows="5"></textarea>
+                    </div>
+                    <div class="mb-2" id="fileArea">
+                        <label for="contentBody">Content</label>
+                        <input type="file" name="contentBody" id="" class="form-control">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+          </div>
+        </div>
+    </div>
+    {{--topic modal end--}}
+    @endif
+
     <div class="row bg-body rounded p-2 m-2">
         <div class="col-5">
             @if ($course->course_image == null)
@@ -59,4 +123,33 @@
         <h6>There are no topic rightnow.</h6>
     </div>
     @endif
+@endsection
+
+@section('J_Script')
+    <script>
+        ClassicEditor
+            .create(document.querySelector('#textContent'))
+            .then(editor => {
+            console.log('Editor initialized:', editor);
+            // You can use the 'editor' instance for further operations
+            })
+            .catch(error => {
+            console.error('Error initializing the editor:', error);
+            });
+
+        $(document).ready(function(){
+            $('#fileArea').hide();
+            $('#selectContentType').on('change', function(){
+                let typeSelected = $(this).val();
+                console.log(typeSelected);
+                if (typeSelected == 'text') {
+                    $('#textArea').show();
+                    $('#fileArea').hide();
+                } else {
+                    $('#fileArea').show();
+                    $('#textArea').hide();
+                }
+            });
+        });
+    </script>
 @endsection
--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -64,6 +64,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.js" integrity="sha512-+k1pnlgt4F1H8L7t3z95o3/KO+o78INEcXTbnoJQ/F2VqDVhWoaiVml/OEHv9HsVgxUaVW+IbiZPUJQfF/YxZw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+    {{-- for  ckeditor --}}
+    <script src="{{ asset('school/js/ckeditor.js') }}"></script>
 
     @yield('J_Script')
 </body>
